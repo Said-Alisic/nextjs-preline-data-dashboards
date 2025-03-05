113bd2e707febc75b151b071fa0fa5e0fb92d12e
said-alisic <45482255+Said-Alisic@users.noreply.github.com>
2025-03-05 22:06:50 +0100

    refactor: 💡 Change area chart tooltip on legend click

--- src/libs/components/charts/AreaChart.tsx
+++ src/libs/components/charts/AreaChart.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import dynamic from "next/dynamic";
 import type { ApexOptions } from "apexcharts";
 
@@ -22,6 +22,8 @@ const AreaChart: FC<ChartProps> = ({
   title,
   customLegendItems,
 }) => {
+  const [isToolTipShared, setIsToolTipShared] = useState(true);
+
   const arrowTrendingUpIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#1fdb6d" class="size-5">
                                    <path fill-rule="evenodd" d="M12.577 4.878a.75.75 0 0 1 .919-.53l4.78 1.281a.75.75 0 0 1 .531.919l-1.281 4.78a.75.75 0 0 1-1.449-.387l.81-3.022a19.407 19.407 0 0 0-5.594 5.203.75.75 0 0 1-1.139.093L7 10.06l-4.72 4.72a.75.75 0 0 1-1.06-1.061l5.25-5.25a.75.75 0 0 1 1.06 0l3.074 3.073a20.923 20.923 0 0 1 5.545-4.931l-3.042-.815a.75.75 0 0 1-.53-.919Z" clip-rule="evenodd" />
                                  </svg>`;
@@ -37,6 +39,11 @@ const AreaChart: FC<ChartProps> = ({
       type: "area",
       toolbar: { show: false },
       zoom: { enabled: false },
+      events: {
+        legendClick: () => {
+          setIsToolTipShared(!isToolTipShared);
+        },
+      },
     },
     dataLabels: { enabled: false },
     stroke: {
@@ -96,7 +103,7 @@ const AreaChart: FC<ChartProps> = ({
       custom: ({ series, dataPointIndex, w }) => {
         const tooltipTitle = title;
 
-        if (series.length > 1 && shouldCompare) {
+        if (series.length > 1 && shouldCompare && isToolTipShared) {
           // Extract the relevant values for comparison
           const currentYearValue = series[0][dataPointIndex] || 0;
           const previousYearValue = series[1][dataPointIndex] || 0;
