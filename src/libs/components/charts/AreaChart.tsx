13dd076b1d617202a9e927c87b451a05c43ab5c0
said-alisic <45482255+Said-Alisic@users.noreply.github.com>
2025-03-06 19:20:01 +0100

    fix: 🐛 Show correct series data when toggling legend on chart

--- src/libs/components/charts/AreaChart.tsx
+++ src/libs/components/charts/AreaChart.tsx
@@ -23,6 +23,7 @@ const AreaChart: FC<ChartProps> = ({
   customLegendItems,
 }) => {
   const [isToolTipShared, setIsToolTipShared] = useState(true);
+  const [hiddenSeriesIndexes, setHiddenSeriesIndexes] = useState<number[]>([]);
 
   const arrowTrendingUpIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#1fdb6d" class="size-5">
                                    <path fill-rule="evenodd" d="M12.577 4.878a.75.75 0 0 1 .919-.53l4.78 1.281a.75.75 0 0 1 .531.919l-1.281 4.78a.75.75 0 0 1-1.449-.387l.81-3.022a19.407 19.407 0 0 0-5.594 5.203.75.75 0 0 1-1.139.093L7 10.06l-4.72 4.72a.75.75 0 0 1-1.06-1.061l5.25-5.25a.75.75 0 0 1 1.06 0l3.074 3.073a20.923 20.923 0 0 1 5.545-4.931l-3.042-.815a.75.75 0 0 1-.53-.919Z" clip-rule="evenodd" />
@@ -40,7 +41,21 @@ const AreaChart: FC<ChartProps> = ({
       toolbar: { show: false },
       zoom: { enabled: false },
       events: {
-        legendClick: () => {
+        legendClick: (chartContext, seriesIndex, config) => {
+          // Check if seriesIndex is defined
+          if (seriesIndex !== undefined) {
+            // Check if the index already exists in the hidden array
+            if (hiddenSeriesIndexes.includes(seriesIndex)) {
+              // If it exists, remove it (series will be shown)
+              setHiddenSeriesIndexes(
+                hiddenSeriesIndexes.filter((index) => index !== seriesIndex)
+              );
+            } else {
+              // If it doesn't exist, add it (series will be hidden)
+              setHiddenSeriesIndexes([...hiddenSeriesIndexes, seriesIndex]);
+            }
+          }
+
           setIsToolTipShared(!isToolTipShared);
         },
       },
@@ -94,11 +109,12 @@ const AreaChart: FC<ChartProps> = ({
     yaxis: {
       labels: {
         style: { colors: "#9ca3af", fontSize: "12px", fontWeight: 400 },
-        formatter: (val) => (val >= 1000 ? `${val / 1000}k` : val.toString()),
+        formatter: (value) =>
+          value >= 100 ? `${value / 100}k` : value.toString(),
       },
     },
     tooltip: {
-      shared: false,
+      shared: isToolTipShared,
       intersect: false,
       custom: ({ series, dataPointIndex, w }) => {
         const tooltipTitle = title;
@@ -128,6 +144,8 @@ const AreaChart: FC<ChartProps> = ({
           let seriesHtml = "";
 
           w.config.series.forEach((s: any, seriesIdx: number) => {
+            if (hiddenSeriesIndexes.includes(seriesIdx)) return;
+
             const color = w.globals.colors[seriesIdx];
             const val = series[seriesIdx][dataPointIndex] || 0;
 
@@ -152,9 +170,14 @@ const AreaChart: FC<ChartProps> = ({
             ${seriesHtml}
           </div>`;
         } else {
-          const singleSeries = w.config.series[0];
-          const singleValue = series[0][dataPointIndex] || 0;
-          const singleColor = w.globals.colors[0];
+          let seriesIndex = 0;
+
+          if (hiddenSeriesIndexes.includes(0)) seriesIndex = 1;
+          else if (hiddenSeriesIndexes.includes(1)) seriesIndex = 0;
+
+          const singleSeries = w.config.series[seriesIndex];
+          const singleValue = series[seriesIndex][dataPointIndex] || 0;
+          const singleColor = w.globals.colors[seriesIndex];
 
           const seriesHtml = `
             <div class="flex items-center pb-2 gap-4">
